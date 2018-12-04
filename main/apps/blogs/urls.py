f8575acc11bb629ae15e16715809bb1a9c64ab67
Raul <Takethefirststep@Rauls-iMac.local>
2018-12-03 16:53:31 -0800

    updated

--- main/apps/blogs/urls.py
+++ main/apps/blogs/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index),
-    url(r'^create_blog$', views.create_blog)
+    url(r'^$', views.index),
+    url(r'^create_blog/$', views.create_blog)
 
 ]
